2f6f95754f03dfbe21fe86176407cb4f507c6433
doruksahin <doruk.sahin@inavitas.com>
2021-11-21 15:27:44 +0300

    generate script and greater analyse added.

--- app/service.js
+++ app/service.js
@@ -37,11 +37,58 @@ async function getGeneralAnalyseSummoner() {
     return j;
 }
 
+
+function generateDetailedAnalyseScript(mana, rule, red, green, blue, black, white, gold) {
+    let script = `select * from battles`;
+    const params = [
+        { name: 'mana', operator: '=', value: mana },
+        { name: 'rule', operator: 'like', value: rule ? `'%${rule}%'` : rule },
+        { name: 'red', operator: '=', value: red },
+        { name: 'blue', operator: '=', value: blue },
+        { name: 'green', operator: '=', value: green },
+        { name: 'white', operator: '=', value: white },
+        { name: 'black', operator: '=', value: black },
+        { name: 'gold', operator: '=', value: gold },
+    ]
+    if (mana || rule || red || green || blue || black || white || gold) script += ` where `;
+    for (const param of params) {
+        if (param.value) {
+            script += ` ${param.name} ${param.operator} ${param.value} and`;
+        }
+    }
+    script = script.slice(0, -3); // deleting last "and" 
+    return script;
+}
+
+function getSortedMap(winnerMinions, firstNth) {
+    // Create items array
+    var winnerMinionsArray = Object.keys(winnerMinions).map(key => {
+        return [key, winnerMinions[key]];
+    });
+
+    // Sort the array based on the second element
+    winnerMinionsArray.sort((first, second) => {
+        return second[1] - first[1];
+    });
+    if (firstNth && winnerMinionsArray.length > firstNth) {
+        winnerMinionsArray = winnerMinionsArray.slice(0, firstNth);
+    }
+    const prunedWinnerMinions = {};
+    winnerMinionsArray.forEach(minion => {
+        prunedWinnerMinions[minion[0]] = minion[1];
+    });
+    return prunedWinnerMinions;
+}
+
 async function getDetailedAnalyse(mana, rule, red, green, blue, black, white, gold) {
-    const winners = [];
+    const result = {};
+    result['winnerDecks'] = [];
+    result['winnerSummoners'] = {};
+    result['winnerMinions'] = {};
     const cardMap = await getCardIdNameMap();
     try {
-        const uniqueBattleQuery = await client.query(scripts.getUniqueBattle, [mana, rule, red, green, blue, black, white, gold]);
+        const script = generateDetailedAnalyseScript(mana, rule, red, green, blue, black, black, white, gold);
+        const uniqueBattleQuery = await client.query(script, []);
         for (const battle of uniqueBattleQuery.rows) {
             const winner = {};
             winner['minions'] = [];
@@ -51,17 +98,24 @@ async function getDetailedAnalyse(mana, rule, red, green, blue, black, white, go
                 if (card.is_winner) {
                     if (card.is_summoner) {
                         winner['summoner'] = cardName;
+                        result['winnerSummoners'][cardName] ? result['winnerSummoners'][cardName] += 1 : result['winnerSummoners'][cardName] = 1;
                     } else {
                         winner['minions'].push(cardName);
+                        const key = `${cardName}-${card.position}`;
+                        result['winnerMinions'][key] ? result['winnerMinions'][key] += 1 : result['winnerMinions'][key] = 1;
+
                     }
                 }
             });
-            winners.push(winner);
+            result['winnerDecks'].push(winner);
         }
+        result['winnerSummoners'] = getSortedMap(result['winnerSummoners']);
+        result['winnerMinions'] = getSortedMap(result['winnerMinions'], 5);
     } catch (e) {
         return e;
     }
-    return winners;
+
+    return result;
 }
 
 module.exports = {
